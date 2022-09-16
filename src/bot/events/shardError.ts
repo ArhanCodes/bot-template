50cf13bd7380842439545bc4da42c9427ddc6616
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-09-16 19:31:13 +0400

    Create shardError.ts

---
+++ src/bot/events/shardError.ts
@@ -0,0 +1,15 @@
+import EventHandler from "../../../lib/classes/EventHandler.js";
+
+export default class ShardError extends EventHandler {
+    override async run(error: Error) {
+        this.client.logger.error(error);
+        const haste = await this.client.functions.uploadHaste(
+            `${error.name}: ${error.message}`
+        );
+        return this.client.logger.webhookLog("console", {
+            content: `${this.client.functions.generateTimestamp()} Shard ${
+                this.client.shard?.ids[0]
+            } encountered an error: ${haste}`
+        });
+    }
+}
